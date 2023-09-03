bb2995caf3a2a74cfaf83f1fda3f523a20a26ff3
Malekmontanaimam <malekimam123@gmail.com>
2023-09-03 11:48:40 +0200

    migrate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -652,6 +652,7 @@
             color: rgb(255 255 255 / var(--tw-text-opacity))
         }
 
+
         .selection\:bg-red-500::selection {
             --tw-bg-opacity: 1;
             background-color: rgb(239 68 68 / var(--tw-bg-opacity))
@@ -836,12 +837,12 @@
         @if (Route::has('login'))
             <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                 @auth
-                    <a href="{{ url('/home') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                    <a href="{{ url('/dashboard') }}"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                 @else
                     <a href="{{ route('login') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                        in</a>
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                        Sign-in</a>
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
