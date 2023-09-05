7db1ec3c5875f0e1e49a00481df04a380b2ad061
Malekmontanaimam <malekimam123@gmail.com>
2023-09-05 14:51:05 +0200

    migrate

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateAvatarRequest;
+
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request){
 
 
 
-        dd($request->all());
-                   return back()->with(['message'=>'Avatar is done']);
+       $path= $request->file('avatar')->store('avatar','public');
+
+       if($oldAvatar=auth()->user()->avatar){
+        Storage::disk('public')->delete($oldAvatar);
+       }
+       auth()->user()->update(['avatar' => $path]);
+                   return redirect(route('profile.edit'))->with('message','avatar is done ');
     }
 }
--- resources/views/profile/partials/user-avatar-form.blade.php
+++ resources/views/profile/partials/user-avatar-form.blade.php
@@ -3,6 +3,7 @@
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             User Avatar
         </h2>
+        <img class='w-8 h-8 rounded-full' src="{{ "/storage/$user->avatar" }}" alt="avatar img" />
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             Update your avatar or add
